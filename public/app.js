fcd851926d6742e58c7b5018d038ffb4d0609fa5
AriGute <arielguata@gmail.com>
2021-08-19 18:23:36 +0300

    Wrap up

--- public/app.js
+++ public/app.js
@@ -1,14 +1,22 @@
 import { Invoice } from "./classes/invoice.js";
+import { ListTemplate } from "./classes/ListTemplate.js";
+import { Payment } from "./classes/payment.js";
 const invOne = new Invoice('Ariel', 'car accident (sorry)', 10001.5);
-let invoices = [];
-invoices.push(invOne);
-console.log(invoices);
 const paymentForm = document.querySelector('form');
 const type = document.querySelector('#type');
 const details = document.querySelector('#details');
 const toFrom = document.querySelector('#to_from');
 const amount = document.querySelector('#amount');
+const ul = document.querySelector('#paylist');
+const list = new ListTemplate(ul);
 paymentForm === null || paymentForm === void 0 ? void 0 : paymentForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, details.value, toFrom.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(toFrom.value, details.value, parseInt(amount.value));
+    }
+    else {
+        doc = new Payment(toFrom.value, details.value, parseInt(amount.value));
+    }
+    list.render(doc, type.value, 'end');
 });
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,9 @@
 import { Invoice } from "./classes/invoice.js";
-
+import { ListTemplate } from "./classes/ListTemplate.js";
+import { Payment } from "./classes/payment.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js"
 
 const invOne = new Invoice('Ariel', 'car accident (sorry)', 10001.5);
-
-let invoices: Invoice[] = [];
-invoices.push(invOne);
-console.log(invoices);
 
 const paymentForm = document.querySelector('form');
 
@@ -14,8 +12,17 @@ const details = document.querySelector('#details') as HTMLInputElement;
 const toFrom = document.querySelector('#to_from') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
+const ul = document.querySelector('#paylist') as HTMLUListElement;
+const list = new ListTemplate(ul);
 
 paymentForm?.addEventListener('submit', (e: Event) => {
     e.preventDefault();
-    console.log(type.value, details.value, toFrom.value, amount.valueAsNumber);
+
+    let doc: HasFormatter;
+    if(type.value === 'invoice'){
+        doc = new Invoice(toFrom.value, details.value, parseInt(amount.value));
+    }else{
+        doc = new Payment(toFrom.value, details.value, parseInt(amount.value));
+    }
+    list.render(doc, type.value, 'end');
 });
